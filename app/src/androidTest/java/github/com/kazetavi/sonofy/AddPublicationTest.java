96bc9ddc1eefb631ddefd323fa7104d1a98fddae
Lamia <lamiakais1449@gmail.com>
2020-11-12 14:21:16 +0100

    Publication UI test added OK & Likes and Dislikes UI test added KO

--- app/src/androidTest/java/github/com/kazetavi/sonofy/AddPublicationTest.java
+++ app/src/androidTest/java/github/com/kazetavi/sonofy/AddPublicationTest.java
@@ -18,56 +18,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class AddPublicationTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
-        ViewInteraction linearLayout = onView(
-                allOf(withId(R.id.likeButton),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.publicationRecyclerView),
-                                        0),
-                                2),
-                        isDisplayed()));
-        linearLayout.perform(click());
-
-        ViewInteraction linearLayout2 = onView(
-                allOf(withId(R.id.dislikeButton),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.publicationRecyclerView),
-                                        0),
-                                3),
-                        isDisplayed()));
-        linearLayout2.perform(click());
-
-        /*ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.publicationRecyclerView),
-                        childAtPosition(
-                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                1)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
-
-        pressBack();*/
-
+    public void mainActivityTest3() {
         ViewInteraction floatingActionButton = onView(
                 allOf(withId(R.id.newPublicationButton),
                         childAtPosition(
@@ -86,7 +53,7 @@ public class MainActivityTest {
                                         0),
                                 2),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("TEST"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("hello"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.youtubeVideoIdEditText),
@@ -96,7 +63,17 @@ public class MainActivityTest {
                                         0),
                                 4),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("Qz6XNSB0F3E"), closeSoftKeyboard());
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.youtubeVideoIdEditText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("t7xHamn5inQ"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton = onView(
                 allOf(withId(R.id.publierButton), withText("Publier"),
