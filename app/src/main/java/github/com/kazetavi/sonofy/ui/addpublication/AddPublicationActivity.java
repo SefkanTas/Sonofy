7f05a8f20bf356095d9439f391030644293ed4fd
LinaZegadi <lynamadjda@gmail.com>
2020-11-23 12:39:05 +0100

    Add URL

--- app/src/main/java/github/com/kazetavi/sonofy/ui/addpublication/AddPublicationActivity.java
+++ app/src/main/java/github/com/kazetavi/sonofy/ui/addpublication/AddPublicationActivity.java
@@ -6,11 +6,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import github.com.kazetavi.sonofy.R;
 import github.com.kazetavi.sonofy.ui.main.MainActivity;
@@ -71,10 +75,17 @@ public class AddPublicationActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(!youtubeVideoIdEditText.getText().toString().isEmpty() && !titreMusiqueEditText.getText().toString().isEmpty()){
                     String titre;
-                    String videoId;
+                    String videoId = null;
 
                     titre = titreMusiqueEditText.getText().toString().trim();
-                    videoId = youtubeVideoIdEditText.getText().toString().trim();
+                    String url = youtubeVideoIdEditText.getText().toString().trim();
+                    String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
+
+                    Pattern compiledPattern = Pattern.compile(pattern);
+                    Matcher matcher = compiledPattern.matcher(url); //url is youtube url for which you want to extract the id.
+                    if (matcher.find()) {
+                        videoId = matcher.group();
+                    }
 
                     addPublicationViewModel.addPublication(titre, videoId);
                 }
--- app/src/main/java/github/com/kazetavi/sonofy/ui/publication/Config.java
+++
@@ -1,8 +0,0 @@
-package github.com.kazetavi.sonofy.ui.publication;
-
-public class Config {
-    private Config() {
-    }
-
-    public static final String YOUTUBE_API_KEY = "AIzaSyBMRPGjEwSrJTjTWAxr1c_P8Uig0b6h9h0";
-}
