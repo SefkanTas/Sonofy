f325737a6203a92d936986c37d1b0d472ccea550
anisoaravida <Olaf120697>
2020-11-23 17:52:19 +0100

    Sort Publication

--- app/src/main/java/github/com/kazetavi/sonofy/ui/main/PublicationAdapter.java
+++ app/src/main/java/github/com/kazetavi/sonofy/ui/main/PublicationAdapter.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
