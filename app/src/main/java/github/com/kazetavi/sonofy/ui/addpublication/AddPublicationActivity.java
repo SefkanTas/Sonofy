89703ec22b9bfc08d1d8d2bd69f23406f6462dda
LinaZegadi <lynamadjda@gmail.com>
2020-11-23 18:59:15 +0100

    Add Youtube URL

--- app/src/main/java/github/com/kazetavi/sonofy/ui/addpublication/AddPublicationActivity.java
+++ app/src/main/java/github/com/kazetavi/sonofy/ui/addpublication/AddPublicationActivity.java
@@ -78,14 +78,7 @@ public class AddPublicationActivity extends AppCompatActivity {
                     String videoId = null;
 
                     titre = titreMusiqueEditText.getText().toString().trim();
-                    String url = youtubeVideoIdEditText.getText().toString().trim();
-                    String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
-
-                    Pattern compiledPattern = Pattern.compile(pattern);
-                    Matcher matcher = compiledPattern.matcher(url); //url is youtube url for which you want to extract the id.
-                    if (matcher.find()) {
-                        videoId = matcher.group();
-                    }
+                    videoId = youtubeVideoIdEditText.getText().toString().trim();
 
                     addPublicationViewModel.addPublication(titre, videoId);
                 }
