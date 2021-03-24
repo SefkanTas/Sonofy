f02680f7d464233bb6cd6125c58ad6a99ce360fe
Sefkan <tas.sefkan@gmail.com>
2021-03-24 19:32:14 +0100

    remove comment related stuff

--- app/src/main/java/github/com/kazetavi/sonofy/ui/publication/PublicationViewModel.java
+++ app/src/main/java/github/com/kazetavi/sonofy/ui/publication/PublicationViewModel.java
@@ -1,23 +1,15 @@
 package github.com.kazetavi.sonofy.ui.publication;
 
-import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import github.com.kazetavi.sonofy.data.api.EmotionFirestore;
 import github.com.kazetavi.sonofy.data.api.PublicationFirestore;
 import github.com.kazetavi.sonofy.data.api.UserFirestore;
-import github.com.kazetavi.sonofy.data.model.Commentaire;
 import github.com.kazetavi.sonofy.data.model.Emotion;
 import github.com.kazetavi.sonofy.data.model.Publication;
 import github.com.kazetavi.sonofy.data.model.User;
@@ -25,7 +17,6 @@ import github.com.kazetavi.sonofy.data.model.User;
 public class PublicationViewModel extends ViewModel {
 
     private final MutableLiveData<Publication> publicationLiveData = new MutableLiveData<>();
-    private final MutableLiveData<List<Commentaire>> commentaires = new MutableLiveData<>();
     private final MutableLiveData<List<Emotion>> emotionsLiveData = new MutableLiveData<>();
     private final MutableLiveData<User> authorUserLiveData = new MutableLiveData<>();
     private final MutableLiveData<User> currentUserLiveData = new MutableLiveData<>();
@@ -38,23 +29,12 @@ public class PublicationViewModel extends ViewModel {
         return emotionsLiveData;
     }
 
-    public MutableLiveData<List<Commentaire>> getCommentaires() {
-        return commentaires;
-    }
-
-    public MutableLiveData<User> getAuthorUserLiveData() {
-        return authorUserLiveData;
-    }
-
     public void loadPublication(final String publicationId){
-        PublicationFirestore.getPublication(publicationId).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                Publication publication = documentSnapshot.toObject(Publication.class);
-                publicationLiveData.postValue(publication);
-                loadAuthorUser(publication);
-                loadEmotions(publicationId);
-            }
+        PublicationFirestore.getPublication(publicationId).addOnSuccessListener(documentSnapshot -> {
+            Publication publication = documentSnapshot.toObject(Publication.class);
+            publicationLiveData.postValue(publication);
+            loadAuthorUser(publication);
+            loadEmotions(publicationId);
         });
     }
 
@@ -62,16 +42,13 @@ public class PublicationViewModel extends ViewModel {
 
         final List<Emotion> emotionList = new ArrayList<>();
 
-        EmotionFirestore.getByPublication(publicationId).addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                emotionList.clear();
-                for (QueryDocumentSnapshot documentSnapshot : value){
-                    Emotion emotion = documentSnapshot.toObject(Emotion.class);
-                    emotionList.add(emotion);
-                }
-                emotionsLiveData.postValue(emotionList);
+        EmotionFirestore.getByPublication(publicationId).addSnapshotListener((value, error) -> {
+            emotionList.clear();
+            for (QueryDocumentSnapshot documentSnapshot : value){
+                Emotion emotion = documentSnapshot.toObject(Emotion.class);
+                emotionList.add(emotion);
             }
+            emotionsLiveData.postValue(emotionList);
         });
     }
 
